cbfd3850a29d3e3900f648826c8d0d1eafd2c305
LYH513 <peperony367@gmail.com>
2025-04-17 17:51:05 +0900

    project setting

---
+++ src/main/java/com/example/crudmemoback/CrudMemoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudmemoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMemoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudMemoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudmemoback/CrudMemoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudmemoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMemoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
